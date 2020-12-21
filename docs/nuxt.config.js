fabfa8d75daec7eeb0fa027700b9119847d96bd8
sent <66543853+sent@users.noreply.github.com>
2020-12-22 00:03:28 +0300

    New documentation

---
+++ docs/nuxt.config.js
@@ -0,0 +1,13 @@
+import theme from '@nuxt/content-theme-docs'
+
+export default theme({
+    /*
+     ** Nuxt target
+     ** See https://nuxtjs.org/api/configuration-target
+     */
+    target: 'static',
+    loading: { color: '#1e429f' },
+    router: {
+        trailingSlash: true,
+    },
+})
